ac6ee97f8507b4aa44ff88b8ee01628f39dbfb98
Charlie <cciordia.b@gmail.com>
2023-12-06 22:07:34 +0100

    streamLIT 🔥

--- streamlit/pages/home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def app():
-    st.title("Home Page")
-    # Resto de tu código para la página de inicio
-
--- streamlit/pages/maps.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def app():
-    st.title("Página de Mapas")
-    # Tu código para la página de mapas aquí, incluyendo la visualización de Foursquare Studio
-    url_foursquare = "https://studio.foursquare.com/public/8729d261-aaf7-4cf6-ad1d-919a41da54d1"
-    st.components.v1.iframe(url_foursquare, width=800, height=600)
-
