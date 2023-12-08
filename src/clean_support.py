6e02fb31ad0e52c4de943662ce365b74db25e22f
Charlie <cciordia.b@gmail.com>
2023-12-08 02:44:22 +0100

    🪐

--- src/clean_support.py
+++ src/clean_support.py
@@ -8,23 +8,21 @@ def NO2_clean(df):
 
     '''
     
-    # Filtrar por la magnitud correspondiente (en este caso, MAGNITUD == 8)
+    # Filtro por MAGNITUD == 8, que es el valor de NO2)
     df = df[df["MAGNITUD"] == 8]
 
-    # Eliminar columnas que contienen 'V'
+    # Elimino columnas con V
     df = df.drop(columns=[e for e in df.columns if 'V' in e])
 
-    # Eliminar columnas innecesarias
     df.drop(['MUNICIPIO', 'MAGNITUD', 'PUNTO_MUESTREO'], axis=1, inplace=True)
 
-    # Convertir a formato largo (melt)
+    # MELT
     df = pd.melt(df, id_vars=['ESTACION', 'ANO', 'MES'], var_name='DIA', value_name='NO2')
 
-    # Convertir la columna 'DIA' a tipo int
     df['DIA'] = df['DIA'].str[1:].astype(int)
 
-    # Calcular el promedio mensual por estación, año y mes
     df = df.groupby(['ESTACION', 'ANO', 'MES'])['NO2'].agg(lambda x: x.mean())
+    
     df = df.reset_index()
 
     return df
